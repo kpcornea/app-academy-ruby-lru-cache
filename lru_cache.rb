58ed3918426f7aac0a6ffd5ed3186dc3d5ef580b
Kevin Cornea <kpcornea@gmail.com>
2018-12-18 00:22:43 -0800

    Refactor initialize to account for length

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,7 +1,8 @@
 class LRUCache
 
-  def initialize
-    @cache = []
+  def initialize(length)
+    # need to take into account original length of cache and resizing i think. yeah i think that deletion will be part of the add method
+    @cache = Array.new(length)
   end
 
   def count
@@ -11,6 +12,7 @@ class LRUCache
 
   def add(el)
     # adds element to cache according to LRU principle
+    # need to take into account nil autofill on initializing array and what happens when reaching max capacity (lru gets deleted)
     if @cache.include?(el)
       @cache.delete(el)
       @cache << el
